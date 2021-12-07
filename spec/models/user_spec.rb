5c7640b206d21bbb1018a3fb0d801c837ef0dc49
Ivan Reyes <ivanwilf@hotmail.com>
2021-12-07 22:32:06 +0900

    refactored spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "Should have a password" do
+  it "Should be invalid without a password" do
     expect { 
       user = create(:no_password_user, email: 'example@me.com')
     }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Password can't be blank")
   end
 
-  it "Should have an api token" do
+  it "Should be invalid without an api token" do
     expect {
       user = create(:no_api_token_user, email: 'example@me.com')
     }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Api token can't be blank")
   end
-
 end
