f9598d0f3a5d9aefe1a92b7d93df1e0e75c885cf
Ivan Reyes <ivanwilf@hotmail.com>
2021-12-07 22:00:24 +0900

    Added api test to user rspec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,11 +2,17 @@ FactoryBot.define do
   factory :user do
     email { "ivan@me.com" }
     password  { "password" }
+    api_token { "123456789abcdef" }
 
     trait :no_password do
       password { nil }
     end
 
+    trait :no_api_token do
+      api_token { nil }
+    end
+
     factory :no_password_user, traits: [:no_password]
+    factory :no_api_token_user, traits: [:no_api_token]
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe User, type: :model do
       user = create(:no_password_user, email: 'example@me.com')
     }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Password can't be blank")
   end
+
+  it "Should have an api token" do
+    expect {
+      user = create(:no_api_token_user, email: 'example@me.com')
+    }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Api token can't be blank")
+  end
+
 end
