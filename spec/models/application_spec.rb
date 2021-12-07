6c1578fa75f7af85f982c89e323c8619b45a882d
Ivan Reyes <ivanwilf@hotmail.com>
2021-12-08 00:02:49 +0900

    Added Application model and wrote some rspecs

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Application, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let (:user) do
+    create(:user)
+  end
+
+  it "Should not allow to create another app with the same name" do
+    app_1 = create(:application, user: user, name: "My application")
+    expect {
+      app_2 = create(:application, user: user, name: "My application")
+    }.to raise_error(ActiveRecord::RecordInvalid,"Validation failed: Name You already have an app with that name.")
+  end
 end
