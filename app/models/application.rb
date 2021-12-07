7c02eddf98ecb083c04996f65e0a9e886ffcc7f1
Ivan Reyes <ivanwilf@hotmail.com>
2021-12-08 00:13:47 +0900

    refactor code and added forgotten route

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,5 +2,5 @@ class Application < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true, uniqueness: { scope: :user_id,
-                                                message: "You already have an app with that name."}
+                                                 message: "You already have an app with that name."}
 end
