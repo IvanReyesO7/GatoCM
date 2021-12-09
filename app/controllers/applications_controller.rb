316570da54194db7b15dd82420552569934f1ba2
Ivan Reyes <ivanwilf@hotmail.com>
2021-12-09 15:03:56 +0900

    Added some tests for controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -6,24 +6,31 @@ class ApplicationsController < ApplicationController
   end
 
   def show
-    if User.find(params[:user_id]) == current_user
+    if User.find(params[:user_id])
       @application = Application.find(application_params[:id])
+    else
+      raise ActiveRecord::RecordNotFound.new
     end
   end
   
   def new
-    @Application = Application.new
+    @application = Application.new
   end
 
   def create
-    @Application = Application.new(application_params)
-    @Application.valid? ? @Application.save! : raise
+    @application = Application.new(application_params)
+    @application.valid? ? @Application.save! : raise
   end
 
   def update
+    @application = Application.find(application_params[:id])
+    @application.update(application_params)
   end
 
   def destroy
+    @application = Application.find(application_params[:id])
+    @application.destroy
+    redirect_to user_applications_path
   end
 
   private
--- spec/requests/applications_spec.rb
+++ spec/requests/applications_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "Applications", type: :request do
+
+  let (:user) do
+    user = create(:user)
+  end
+
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "Should show user their created apps" do
+      get user_applications_path(user_id: user.id)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET /show" do
+    it "Should let you access your own apps" do
+      app = create(:application, user: user)
+      get "/users/#{user.id}/applications/#{app.id}"
+      expect(response).to have_http_status(200)
+    end
   end
 end
