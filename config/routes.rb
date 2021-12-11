ca82fa84239778db21fead94431055d699aad6e1
Ivan Reyes <ivanwilf@hotmail.com>
2021-12-11 14:20:17 +0900

    Added routes and generated lists controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   
   resources :users, param: :username, path: '/', only: [:show, :edit, :update, :destroy] do
-    resources :applications
+    resources :applications, param: :name, path: '/', only: [:show, :edit, :update, :destroy] do
+      resources :lists, param: :name, path: '/lists/', only: [:show, :edit, :update, :destroy] 
+    end
   end
 end
