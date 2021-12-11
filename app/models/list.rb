cc1737eededae9942e6dbb3a5b8b99ca22dd5066
Ivan Reyes <ivanwilf@hotmail.com>
2021-12-11 12:35:11 +0900

    Added list and validation, after migration

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,5 @@
 class List < ApplicationRecord
   belongs_to :application
+  validates :name, presence: true, uniqueness: { scope: :user_id,
+                                                 message: "You already have an list in this app with that name."}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_08_025942) do
+ActiveRecord::Schema.define(version: 2021_12_11_032437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2021_12_08_025942) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_applications_on_user_id"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
+    t.bigint "application_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["application_id"], name: "index_lists_on_application_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +47,5 @@ ActiveRecord::Schema.define(version: 2021_12_08_025942) do
   end
 
   add_foreign_key "applications", "users"
+  add_foreign_key "lists", "applications"
 end
