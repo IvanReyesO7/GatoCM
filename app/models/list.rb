24051ec31e670d3a59c771f37ff83a8406798510
Ivan Reyes <ivanwilf@hotmail.com>
2021-12-13 14:58:44 +0900

    Fixed second typo

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,5 @@
 class List < ApplicationRecord
   belongs_to :application
   validates :name, presence: true, uniqueness: { scope: :application_id,
-                                                 message: "You already have an list in this app with that name."}
+                                                 message: "You already have a list in this app with that name."}
 end
