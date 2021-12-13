19cfecbe0fa47b0a8617b70b8dc5752c42b2a2da
Ivan Reyes <ivanwilf@hotmail.com>
2021-12-13 15:12:34 +0900

    Added items to list model

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List < ApplicationRecord
   belongs_to :application
+  has_many :items
   validates :name, presence: true, uniqueness: { scope: :application_id,
                                                  message: "You already have a list in this app with that name."}
 end
