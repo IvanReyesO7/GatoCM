c657653df4e17bebf41f643dfe454d298f2c44fd
Ivan Reyes <ivanwilf@hotmail.com>
2021-12-22 18:29:55 +0900

    Added validation to Application model

--- app/models/application.rb
+++ app/models/application.rb
@@ -2,6 +2,7 @@ class Application < ApplicationRecord
   belongs_to :user
   has_many :lists
   has_many :images
+  has_many :codes 
   validates :name, presence: true, uniqueness: { scope: :user_id,
                                                  message: "You already have an app with that name."}
 end
