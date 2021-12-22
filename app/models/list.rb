1c4038bc77b305cecb9cc9a82c98eede3c23c592
Ivan Reyes <ivanwilf@hotmail.com>
2021-12-22 23:03:31 +0900

    added abject creation to list

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,10 @@
 class List < ApplicationRecord
   belongs_to :application
   has_many :items
+
   before_create :generate_name_fomat
+  after_save :create_component
+
   validates :name, presence: true, uniqueness: { scope: :application_id,
                                                  case_sensitive: true,
                                                  message: "You already have a list in this app with that name."}
@@ -10,4 +13,14 @@ class List < ApplicationRecord
   def generate_name_fomat
     self.name_format = self.name.downcase.gsub(" ","_")
   end 
+
+  def create_component
+    Component.new.tap do |comp|
+      comp.real_component_type = self.class,
+      comp.real_component_id = self.id,
+      comp.real_component_title = self.name,
+      comp.application = self.application)
+      comp.save!
+    end
+  end
 end
