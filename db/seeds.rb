4ebadae63bbb42d82a128b49410bcdd8bc80d4bf
Ivan Reyes <ivanwilf@hotmail.com>
2021-12-23 14:23:43 +0900

    Added seeds and inputs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+# Creating data for the following users
+usernames = ['Ivan', 'Emanuel', 'Luis', 'David', 'Peruano']
+usernames.each do |username|
+  # Creating Users
+  User.new.tap do |usr|
+    usr.email = "#{username}@email.com"
+    usr.password = "password"
+    usr.username = username
+    usr.save!
+    puts "User: #{username} created"
+    # Create App
+    Application.new.tap do |app|
+      app.name = Faker::App.name
+      app.user = usr
+      app.save!
+      puts "-Application: #{app.name} created for user: #{username}"
+      # Create list
+      List.new.tap do |list|
+        list.name = Faker::JapaneseMedia::DragonBall.character
+        list.application = app
+        list.save!
+        puts "-- List: #{list.name} created"
+        # Create 15 items
+        i = 1
+        15.times { 
+          item = Item.new(list: list, content: Faker::Movies::StarWars.quote)
+          item.save!
+          puts "--- Item ##{i} created for List #{list.name}"
+          i += 1
+        }
+      end
+      # Create Image
+      Image.new.tap do |img|
+        img.title = Faker::Sports::Football.player
+        img.url = "https://#{Faker::Internet.domain_name}"
+        img.application = app
+        img.save!
+        puts "-- Image: #{img.title} created"
+      end
+      #Create Code
+      Code.new.tap do |code|
+        code.title = Faker::Games::Minecraft.mob
+        code.content = "<h1>This is a piece of code for #{code.title}</h1>"
+        code.application = app
+        code.save!
+        puts "-- Code: #{code.title} created"
+      end
+    end
+  end
+end
