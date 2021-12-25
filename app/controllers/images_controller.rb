8b2878eed780383ad292356360b9251a7fe383de
Ivan Reyes <ivanwilf@hotmail.com>
2021-12-25 23:24:38 +0900

    Added show view for Image model

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,11 @@
 class ImagesController < ApplicationController
   def show
-    raise
+    @image = Image.find_by(name_format: images_params[:name_format])
+  end
+
+  private
+
+  def images_params
+    params.permit(:user_username, :application_name, :name_format)
   end
 end
