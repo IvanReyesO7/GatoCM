4714fbc3081e6003ef21ebf2101c398986a849a2
Ivan Reyes <ivanwilf@hotmail.com>
2021-12-26 17:45:03 +0900

    Added test and validation to Item model

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,5 +3,11 @@ FactoryBot.define do
     name { "MyString" }
     content { "MyText" }
     list { nil }
+
+    trait :no_content do
+      content { nil }
+    end
+
+    factory :no_content_item, traits: [:no_content]
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:list) do
+    create(:list)
+  end
+
+  it "Should not allow the creation of an item without content" do
+    expect {
+      create(:no_content_item, list: list)
+    }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Content Item cannot be blank")
+  end
 end
