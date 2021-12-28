30351e6ec00fdfd76655680b0e74f40542891c1d
Ivan Reyes <ivanwilf@hotmail.com>
2021-12-28 13:42:28 +0900

    Added admin_user factory and commented out helper tests beacuse I don't have helpers ;-;

--- spec/requests/applications_spec.rb
+++ spec/requests/applications_spec.rb
@@ -3,7 +3,15 @@ require 'rails_helper'
 RSpec.describe "Applications", type: :request do
 
   let (:user) do
-    user = create(:user)
+    create(:user)
+  end
+
+  let(:user_2) do
+    create(:user, username: "user_2", email: "user_2@me.com")
+  end
+
+  let(:admin) do
+    create(:admin_user, username: "admin", email: "admin@me.com")
   end
 
   describe "GET /index" do
@@ -14,23 +22,35 @@ RSpec.describe "Applications", type: :request do
   end
 
   describe "GET /show" do
+    context "Normal users" do
+      before do
+        sign_in(user)
+      end
 
-    before do
-      sign_in(user)
-    end
-    
-    it "Should let you access your own apps" do
-      app = create(:application, user: user)
-      get "/#{user.username}/#{app.name}"
-      expect(response).to have_http_status(200)
+      it "Should let you access your own apps" do
+        app = create(:application, user: user)
+        get "/#{user.username}/#{app.name}"
+        expect(response).to have_http_status(200)
+      end
+
+      it "Should not let you access other users apps" do
+        app_2 = create(:application, user: user_2)
+        expect do 
+          get "/#{user_2.username}/#{app_2.name}"
+        end.to raise_error(ActiveRecord::RecordNotFound)
+      end
     end
 
-    it "Should not let you access other users apps" do
-      user_2 = create(:user, username: "user_2", email: "user_2@me.com")
-      app_2 = create(:application, user: user_2)
-      expect do 
+    context "admin users" do
+      before do
+        sign_in(admin)
+      end
+
+      it "Should let an admin see other user's apps" do
+        app_2 = create(:application, user: user_2)
         get "/#{user_2.username}/#{app_2.name}"
-      end.to raise_error(ActiveRecord::RecordNotFound)
+        expect(response).to have_http_status(200)
+      end
     end
   end
 end
