fd971cdcf8783421abd7e5d7f6bf78c22309510d
Ivan Reyes <ivanwilf@hotmail.com>
2021-12-28 14:20:29 +0900

    Added tests for lists and codes requests

--- spec/requests/codes_spec.rb
+++ spec/requests/codes_spec.rb
@@ -1,7 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe "Codes", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /show" do
+
+    let (:user) { create(:user) }
+    let (:user_2) { create(:user, username: "user_2", email: "user_2@me.com") }
+    let (:admin) { create(:admin_user, username: "admin", email: "admin@me.com") }
+    
+    context "Normal users" do
+
+      before do
+        sign_in(user)
+      end
+
+      it "Should show you your own codes" do
+        app = create(:application, user: user)
+        code = create(:code, application: app)
+        get "/#{user.username}/#{app.name}/codes/#{code.name_format}"
+        expect(response).to have_http_status(200)
+      end
+
+      it "Should not show you other users codes" do
+        app_2 = create(:application, name: "app_2", user: user_2)
+        code_2 = create(:code, title: "code 2", application: app_2)
+        expect do 
+          get "/#{user_2.username}/#{app_2.name}/codes/#{code_2.name_format}"
+        end.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    context "admin users" do
+      before do
+        sign_in(admin)
+      end
+
+      it "Should allow user admin to see other user codes" do
+        app_2 = create(:application, name: "app_2", user: user_2)
+        code_2 = create(:code, title: "code 2", application: app_2)
+        get "/#{user_2.username}/#{app_2.name}/codes/#{code_2.name_format}"
+        expect(response).to have_http_status(200)
+      end
+    end
   end
 end
