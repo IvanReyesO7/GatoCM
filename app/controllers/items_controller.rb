9680d6f4a4c2d5d1abe216829ac9bb42f6fdd360
Ivan Reyes <ivanwilf@hotmail.com>
2021-12-31 14:15:05 +0900

    Fixed bug, added @application to @list filter

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,8 @@
 class ItemsController < ApplicationController
   
   before_action :select_user_application_list_from_params, only: [:new, :create]
+  before_action :raise_unless_visible, only: [:new, :create]
+
   def new
     @item = Item.new
   end
@@ -10,6 +12,7 @@ class ItemsController < ApplicationController
       content:   item_params[:item][:content],
       list:      @list
     )
+    p @item.valid?
     if @item.save
       redirect_to user_application_list_path(name_format: @list.name_format)
     else
@@ -25,7 +28,7 @@ class ItemsController < ApplicationController
 
   def select_user_application_list_from_params
     @user = User.find_by(username: item_params[:user_username])
-    @application = Application.find_by(name: item_params[:application_name], user: @user)
-    @list = List.find_by(name_format: item_params[:list_name_format])
+    @application = Application.find_by(user: @user, name: item_params[:application_name])
+    @list = List.find_by(name_format: item_params[:list_name_format], application: @application)
   end
 end
