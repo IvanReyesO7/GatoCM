1caefc30531eac417f9698219a7e980df53fd081
Ivan Reyes <ivanwilf@hotmail.com>
2022-01-03 13:36:53 +0900

    Added tests for list#new, also, added :new to the scope of the visibility policy

--- spec/requests/lists_spec.rb
+++ spec/requests/lists_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe "Lists", type: :request do
   describe "GET /show" do
-
     let (:user) { create(:user) }
     let (:user_2) { create(:user, username: "user_2", email: "user_2@me.com") }
     let (:admin) { create(:admin_user, username: "admin", email: "admin@me.com") }
@@ -38,6 +37,46 @@ RSpec.describe "Lists", type: :request do
         app_2 = create(:application, name: "app_2", user: user_2)
         list_2 = create(:list, name: "list 2", application: app_2)
         get "/#{user_2.username}/#{app_2.name}/lists/#{list_2.name_format}"
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+
+  describe "GET /new" do
+    let (:user) { create(:user) }
+    let (:user_2) { create(:user, username: "user_2", email: "user_2@me.com") }
+    let (:admin) { create(:admin_user, username: "admin", email: "admin@me.com") }
+    
+    context "Normal users" do
+
+      before do
+        sign_in(user)
+      end
+
+      it "Should show you new list form in your own app" do
+        app = create(:application, user: user)
+        get "/#{user.username}/#{app.name}/lists/new"
+        expect(response).to have_http_status(200)
+      end
+
+      it "Should not show you list form in other users lists" do
+        app_2 = create(:application, name: "app_2", user: user_2)
+        list_2 = create(:list, name: "list 2", application: app_2)
+        expect do 
+          get "/#{user_2.username}/#{app_2.name}/lists/new"
+        end.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    context "admin users" do
+      before do
+        sign_in(admin)
+      end
+
+      it "Should allow user admin to see list form in other user apps" do
+        app_2 = create(:application, name: "app_2", user: user_2)
+        list_2 = create(:list, name: "list 2", application: app_2)
+        get "/#{user_2.username}/#{app_2.name}/lists/new"
         expect(response).to have_http_status(200)
       end
     end
