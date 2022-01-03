021bc045bc2174b6ddb0850c2953995c8da6d7c1
Ivan Reyes <ivanwilf@hotmail.com>
2022-01-03 13:27:57 +0900

    Added test to list#create

--- spec/requests/lists_spec.rb
+++ spec/requests/lists_spec.rb
@@ -42,4 +42,57 @@ RSpec.describe "Lists", type: :request do
       end
     end
   end
+
+  describe "POST /create" do
+    let (:user) { create(:user) }
+    let (:user_2) { create(:user, username: "user_2", email: "user_2@me.com") }
+    let (:admin) { create(:admin_user, username: "admin", email: "admin@me.com") }
+    
+    context "Normal users" do
+
+      before do
+        sign_in(user)
+      end
+
+      it "Should allow you to create a list in your own app" do
+        @app = create(:application, user: user)
+        post "/#{user.username}/#{@app.name}/lists/",
+        params: {
+          application_name: @app.name,
+          user_username:    user.username,
+          list:             { name: "Test" }
+        }
+        expect(response.status).to eq(302)
+      end
+
+      it "Should not allow you to create lists in other users apps" do
+        @app = create(:application, user: user_2)
+        expect do
+          post "/#{user_2.username}/#{@app.name}/lists/",
+          params: {
+            application_name: @app.name,
+            user_username:    user_2.username,
+            list:             { name: "Test" }
+          }
+        end.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    context "Admin users" do
+      before do
+        sign_in(admin)
+      end
+
+      it "Should allow admins to create lists in other users apps" do
+        @app = create(:application, user: user)
+        post "/#{user.username}/#{@app.name}/lists/",
+        params: {
+          application_name: @app.name,
+          user_username:    user.username,
+          list:             { name: "Test" }
+        }
+        expect(response.status).to eq(302)        
+      end
+    end
+  end
 end
