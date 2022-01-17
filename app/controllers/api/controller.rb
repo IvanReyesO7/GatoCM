43f5301e0213f365d31fea3d6cca5904ee04ae8d
Ivan Reyes <ivanwilf@hotmail.com>
2022-01-16 23:48:21 -0500

    render json error when not authorized

--- app/controllers/api/controller.rb
+++ app/controllers/api/controller.rb
@@ -1,7 +1,7 @@
 class Api::Controller < ApplicationController
   def raise_unless_valid_api_token
     if !ApplicationApiAuthorizationPolicy.new.valid_api_token?(@token, @user)
-      raise ActiveRecord::RecordNotFound.new
+      render json: {errors: [message: "403 Forbidden"]}, status: 403
     end
   end
 end
