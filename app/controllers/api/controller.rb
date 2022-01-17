27cfb8435d2127e98ddca60f3cb098a46a0eb441
Ivan Reyes <ivanwilf@hotmail.com>
2022-01-16 23:26:58 -0500

    Added basic api authentication

--- app/controllers/api/controller.rb
+++ app/controllers/api/controller.rb
@@ -1,2 +1,7 @@
 class Api::Controller < ApplicationController
+  def raise_unless_valid_api_token
+    if !ApplicationApiAuthorizationPolicy.new.valid_api_token?(@token, @user)
+      raise ActiveRecord::RecordNotFound.new
+    end
+  end
 end
--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -1,11 +1,22 @@
 class Api::V1::ListsController < Api::Controller
+  before_action :select_user_application_list_from_params, only: [:all]
+  before_action :select_token_from_request, only: [:all]
+  before_action :raise_unless_valid_api_token, only: [:all]
+
   def all
-    @user = User.find_by!(username: list_params[:username])
-    @app = Application.find_by!(name: list_params[:application_name], user: @user)
     @lists = @app.lists
   end
 
   def list_params
     params.permit(:api_token, :username, :application_name, :format)
+  end
+
+  def select_user_application_list_from_params
+    @user = User.find_by!(username: list_params[:username])
+    @app = Application.find_by!(name: list_params[:application_name], user: @user)
+  end
+
+  def select_token_from_request
+    @token = request.headers["Authorization"]
   end
 end
