0af1bd25b7cbf7c4aafb82c9050351e85612bb7d
Ivan Reyes <ivanwilf@hotmail.com>
2022-03-08 21:56:22 +0900

    Added test for upload feature

--- spec/requests/lists_spec.rb
+++ spec/requests/lists_spec.rb
@@ -201,27 +201,26 @@ RSpec.describe "Lists", type: :request do
       end
 
       it "Should allow users to create items from an uploaded file" do
-        @file = fixture_file_upload('sentences.json')
+        @file = fixture_file_upload('sentences.json').tap do |file|
+          file.content_type = 'application/json'
+        end
         @app = create(:application, user: user)
         @list = create(:list, application: @app)
-        post "/#{user.username}/#{@app.name}/lists/#{@list.name_format}/import", params: {"list" => {uploaded_file: @file}}
-        expect(@list.items.count).to eq(40)
-      end
-
-      it "Should allow users to create items fredirect after creating items" do
-        @file = fixture_file_upload('sentences.json')
-        @app = create(:application, user: user)
-        @list = create(:list, application: @app)
-        post "/#{user.username}/#{@app.name}/lists/#{@list.name_format}/import", params: {"list" => {uploaded_file: @file}}
+        post user_application_list_import_path(user_username: user.username,
+                                               application_name: @app.name,
+                                               list_name_format: @list.name_format,
+                                               list: {uploaded_file: @file})
         expect(response.status).to eq(302)
       end
+
 
       it "Should not allow users to create items from a file different to JSON or YAML" do
         @file = fixture_file_upload('sentences.html')
         @app = create(:application, user: user)
         @list = create(:list, application: @app)
         post "/#{user.username}/#{@app.name}/lists/#{@list.name_format}/import", params: {"list" => {uploaded_file: @file}}
-        expect(response.status).to raise_error(StandardError)
+        expect(response.status).to eq(302)
+        expect(flash[:alert]).to eq("Something went wrong. Content type not recognized")
       end
     end
   end
