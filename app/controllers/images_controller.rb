79e139b11b99c7983037de29c9a7ae000e988375
Ivan Reyes <ivanwilf@hotmail.com>
2022-03-09 21:36:15 +0900

    Added view for images#new

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,12 +2,17 @@ class ImagesController < ApplicationController
 
   before_action :select_user_application_image_from_params, only: [:show]
   before_action :raise_unless_visible_component, only: [:show]
+  before_action :select_user_application_from_params, only: [:new]
 
   def show
   end
 
   def new
     @image = Image.new
+  end
+
+  def create
+    raise
   end
 
   private
@@ -20,5 +25,10 @@ class ImagesController < ApplicationController
     @user = User.find_by!(username: images_params[:user_username])
     @application = Application.find_by!(user: @user, name: images_params[:application_name])
     @component = @image = Image.find_by!(application: @application ,name_format: images_params[:name_format])
+  end
+
+  def select_user_application_from_params
+    @user = User.find_by!(username: images_params[:user_username])
+    @application = Application.find_by!(user: @user, name: images_params[:application_name])
   end
 end
