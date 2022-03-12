522397ca2f605e9716d8f81d6f71d877e90d0b03
Ivan Reyes <ivanwilf@hotmail.com>
2022-03-12 17:40:15 +0900

    Added view, added methods to make scope of policy cover codes

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -2,6 +2,8 @@ class CodesController < ApplicationController
 
   before_action :select_user_application_code_from_params, only: [:show]
   before_action :raise_unless_visible_component, only: [:show]
+  before_action :select_user_application_from_params, only: [:new, :create]
+  before_action :raise_unless_visible, only: [:create, :new, :destroy]
 
   def show
   end
@@ -20,5 +22,10 @@ class CodesController < ApplicationController
     @user = User.find_by!(username: codes_params[:user_username])
     @application = Application.find_by!(user: @user, name: codes_params[:application_name])
     @component = @code = Code.find_by!(application: @application, name_format: codes_params[:name_format])
+  end
+
+  def select_user_application_from_params
+    @user = User.find_by!(username: codes_params[:user_username])
+    @application = Application.find_by!(user: @user, name: codes_params[:application_name])
   end
 end
