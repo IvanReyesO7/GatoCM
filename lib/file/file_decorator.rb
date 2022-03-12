9b1585e9b7abcdac1ee00360c037595ec3176f1b
Ivan Reyes <ivanwilf@hotmail.com>
2022-03-12 23:15:36 +0900

    Created file decorator in the lib

---
+++ lib/file/file_decorator.rb
@@ -0,0 +1,27 @@
+class FileDecorator
+  def initialize(uploaded_file)
+    @path = uploaded_file.tempfile.path
+    @name = uploaded_file.original_filename
+    @content_type = uploaded_file.content_type
+    @file = File.open(@path)
+    @type = detect_type
+    @content = @file.read
+  end
+
+  def detect_type
+    case @content_type
+    when "text/x-ruby-script"
+      return "ruby"
+    when "text/javascript"
+      return "javascript"
+    when "text/x-python-script"
+      return "python"
+    when "text/html"
+      return "html"
+    when "text/css"
+      return "css"
+    else
+      raise StandardError.new("File type not supported")
+    end 
+  end
+end
