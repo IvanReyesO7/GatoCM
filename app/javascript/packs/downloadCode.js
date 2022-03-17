2bdad10636594760a9aac6dfa71e62c627071c09
Ivan Reyes <ivanwilf@hotmail.com>
2022-03-17 21:10:30 +0900

    Got rid of useless if statement

--- app/javascript/packs/downloadCode.js
+++ app/javascript/packs/downloadCode.js
@@ -1,25 +1,23 @@
 const downloadCode = () => {
   const code = document.querySelector('.code-data');
   const downloadBtn = document.querySelector('.download-btn');
-  if (downloadBtn) {
-    downloadBtn.addEventListener('click', downloadFile);
-    function downloadFile(e) {
-      const codeObject = JSON.parse(code.dataset['code']);
-      const pom = document.createElement('a');
-      // Create the file
-      const content = codeObject['content'];
+  downloadBtn.addEventListener('click', downloadFile);
+  function downloadFile(e) {
+    const codeObject = JSON.parse(code.dataset['code']);
+    const pom = document.createElement('a');
+    // Create the file
+    const content = codeObject['content'];
 
-      pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(content));
-      pom.setAttribute('download', `${codeObject['title']}`);
+    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(content));
+    pom.setAttribute('download', `${codeObject['title']}`);
 
-      if (document.createEvent) {
-        const event = document.createEvent('MouseEvents');
-        event.initEvent('click', true, true);
-        pom.dispatchEvent(event);
-      }
-      else {
-        pom.click();
-      }
+    if (document.createEvent) {
+      const event = document.createEvent('MouseEvents');
+      event.initEvent('click', true, true);
+      pom.dispatchEvent(event);
+    }
+    else {
+      pom.click();
     }
   }
 }
