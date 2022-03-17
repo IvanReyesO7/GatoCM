48f0fdb84fa3c2f6ae82cdefc17926ddf9d86ee5
Ivan Reyes <ivanwilf@hotmail.com>
2022-03-17 21:07:48 +0900

    Organized javascript in file

---
+++ app/javascript/packs/downloadCode.js
@@ -0,0 +1,27 @@
+const downloadCode = () => {
+  const code = document.querySelector('.code-data');
+  const downloadBtn = document.querySelector('.download-btn');
+  if (downloadBtn) {
+    downloadBtn.addEventListener('click', downloadFile);
+    function downloadFile(e) {
+      const codeObject = JSON.parse(code.dataset['code']);
+      const pom = document.createElement('a');
+      // Create the file
+      const content = codeObject['content'];
+
+      pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(content));
+      pom.setAttribute('download', `${codeObject['title']}`);
+
+      if (document.createEvent) {
+        const event = document.createEvent('MouseEvents');
+        event.initEvent('click', true, true);
+        pom.dispatchEvent(event);
+      }
+      else {
+        pom.click();
+      }
+    }
+  }
+}
+  
+downloadCode();
