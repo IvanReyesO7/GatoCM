2919935a3d644a4db7ce59f469ce66bbc3106630
Ivan Reyes <ivanwilf@hotmail.com>
2022-06-06 21:36:50 +0900

    Fixed modal close btn bug

--- app/javascript/packs/renderModalImage.js
+++ app/javascript/packs/renderModalImage.js
@@ -1,25 +1,25 @@
 // Get the modal
-let modal = document.getElementById("myModalImage");
+let modalImage = document.getElementById("myModalImage");
 
 // Get the button that opens the modal
 let btn = document.getElementById("create-image-card");
 
 // Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close-image")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
-  modal.style.display = "block";
+  modalImage.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// When the user clicks on <span> (x), close the modalImage
 span.onclick = function() {
-  modal.style.display = "none";
+  modalImage.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// When the user clicks anywhere outside of the modalImage, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modalImage) {
+    modalImage.style.display = "none";
   }
 }
