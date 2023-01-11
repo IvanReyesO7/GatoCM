b93ea9a6dea49290408678af672998dc76d4eee9
Ivan Reyes <ivanwilf@hotmail.com>
2023-01-11 16:10:54 +0900

    Added tests for new redirect

---
+++ spec/requests/pages_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "Pages", type: :request do
+  describe "describe GET /" do
+    context "Logged in user" do
+      let(:user) { create(:user) }
+
+      before do
+        sign_in(user)
+      end
+
+      it "should redirect to dashboard view" do
+        get "/"
+
+        expect(response).to redirect_to("/#{user.username}")
+      end
+    end
+    context "No session user" do
+      it "should go to /" do
+        get "/"
+
+        expect(response.status).to eq(200)
+      end
+    end
+  end
+end
